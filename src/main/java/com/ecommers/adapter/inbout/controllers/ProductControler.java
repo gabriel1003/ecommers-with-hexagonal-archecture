dba85d39797eea3d4dca52bb596029d9f5aa3211
gabriel1003 <gabriel.samersla97@gmail.com>
2025-02-18 19:46:22 -0300

    feat: adiciona a classe ProductController

---
+++ src/main/java/com/ecommers/adapter/inbout/controllers/ProductControler.java
@@ -0,0 +1,12 @@
+package com.ecommers.adapter.inbout.controllers;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/product")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductControler {
+}
